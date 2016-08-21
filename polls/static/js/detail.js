99253932f37e854422fea798085723912f03c42f
ahmed sakr <ahmed.sakr@carleton.ca>
2016-08-21 17:12:22 -0400

    Code enhancement and improvements

--- polls/static/js/detail.js
+++ polls/static/js/detail.js
@@ -1,42 +1,42 @@
-var selectedChoice = 0;
-
-function jQueryEffects(count) {
+/**
+ * Iterates over all the choice rectangles, and registers all
+ * available listeners.
+ */
+function registerListeners(count) {
     $(document).ready(
         function() {
             for (x = 1; x <= count; x++) {
 
-                // registering the hover effect of the choice boxes
-                registerHoverEffects(x);
-
-                // registering the choice box on-click radio button toggle
-
-                $('#rect-' + x).click(
-                    function() {
-
-                        // unselects the currently selected (if any) choice
-                        // and re-registers the hover effects.
-                        if (selectedChoice != 0) {
-                            registerHoverEffects(selectedChoice);
-                        }
-
-                        // deregisters the hover effects for the selected choice,
-                        // saves the selected choice index, and selects the
-                        // radio button
-                        $(this).hover(function(){ });
-                        selectedChoice = $(this).attr('name').slice(-1);
-
-                        $( '#choice' + selectedChoice).prop('checked', true);
-                });
+                registerHoverListener('#rect-' + x);
+                registerSelectionListener('#rect-' + x);
             }
         });
 }
 
-function registerHoverEffects(choice) {
-    $('#rect-' + choice).hover(
+/**
+ * Hover listener focuses on the main element the user's mouse is currently
+ * hovered on, giving the recetangle a full opacity of 1.0, while all other
+ * rectangles are dimmed to an opacity of 0.7.
+ */
+function registerHoverListener(elem) {
+    $(elem).hover(
         function() {
             $(this).css({opacity: 1});
         },
         function() {
             $(this).css({opacity: 0.7});
+    });
+}
+
+/**
+ * Selection Listener essentially allows the user to click anywhere on the
+ * choice rectangle to toggle the corresponding radio button, rather than having
+ * to navigate to the far-left radio button and selecting it.
+ */
+function registerSelectionListener(elem) {
+    $(elem).click(
+        function() {
+            selectedChoice = $(this).attr('name').slice(-1);
+            $( '#choice' + selectedChoice).prop('checked', true);
     });
 }
