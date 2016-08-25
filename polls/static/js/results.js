33463b5cebee100a8495b0cb11f1555cb5228f98
ahmed sakr <ahmed.sakr@carleton.ca>
2016-08-25 08:38:59 -0400

    Minor homepage style changes and javascript enhancements

--- polls/static/js/results.js
+++ polls/static/js/results.js
@@ -1,23 +1,27 @@
 function loadResults(total_votes, total_choices) {
     var width = 1;
     var i = 1;
-    var elem = document.getElementById("foreground" + i);
-    var value = elem.getAttribute('value');
+    var elem = $("#foreground" + i);
+    var value = elem.attr('value');
     var percentage = ((value / total_votes) * 100).toFixed(1);
 
     var interval = setInterval(function() {
         if (width < percentage) {
             width++;
-            elem.style['width'] = width + '%';
+            elem.css('width',  width + '%');
         } else if (i < total_choices){
-            elem.style['text-indent'] = '0em';
+            elem.css('text-indent',  '0em');
+
+            // moving on to the next choice
             i++;
             width = 1;
-            elem = document.getElementById("foreground" + i);
-            value = elem.getAttribute('value');
+            elem = $("#foreground" + i);
+            value = elem.attr('value');
             percentage = ((value / total_votes) * 100).toFixed(1);
         } else {
-            elem.style['text-indent'] = '0em';
+
+            // all choices has been loaded
+            elem.css('text-indent',  '0em');
             clearInterval(interval);
             $ ('#update').removeAttr('disabled');
         }
