fa98f67fa6878b78089647ab37dbc8ac28464c5f
ahmed sakr <ahmed.sakr@carleton.ca>
2016-09-03 16:41:07 -0400

    Javascript code revision and improvement.

--- polls/static/polls/js/detail.js
+++ polls/static/polls/js/detail.js
@@ -1,37 +1,14 @@
 /**
- * Iterates over all the choice rectangles, and registers all
- * available listeners.
- */
-function registerListeners(count) {
-    $(document).ready(function() {
-        for (x = 1; x <= count; x++) {
-            registerHoverListener('#option-' + x);
-            registerSelectionListener('#option-' + x);
-        }
-    });
-}
-
-/**
- * Hover listener focuses on the main element the user's mouse is currently
- * hovered on, giving the recetangle a full opacity of 1.0, while all other
- * rectangles are dimmed to an opacity of 0.7.
- */
-function registerHoverListener(elem) {
-    $(elem).hover(function() {
-            $(this).css({opacity: 1});
-    }, function() {
-            $(this).css({opacity: 0.7});
-    });
-}
-
-/**
  * Selection Listener essentially allows the user to click anywhere on the
  * choice rectangle to toggle the corresponding radio button, rather than having
  * to navigate to the far-left radio button and selecting it.
  */
-function registerSelectionListener(elem) {
-    $(elem).click(function() {
-        selectedChoice = $(this).attr('id').slice(-1);
-        $( '#radio' + selectedChoice).prop('checked', true);
+function registerSelectionListeners(count) {
+    $(document).ready(function() {
+        for (var x = 1; x <= count; x++) {
+            $('#option-' + x).click(function() {
+                $(this).children().filter('input').prop('checked', true);
+            });
+        }
     });
 }
