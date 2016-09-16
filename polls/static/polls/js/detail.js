813be586837a9d72a325b11f9bfd6a450f1676c8
Ahmed Sakr <ahmed.sakr@carleton.ca>
2016-09-15 21:45:21 -0400

    JS Bug fix and UI enhancement

--- polls/static/polls/js/detail.js
+++ polls/static/polls/js/detail.js
@@ -6,8 +6,9 @@
 function registerSelectionListeners(count) {
     $(document).ready(function() {
         for (var x = 1; x <= count; x++) {
-            $('form').children().filter('div').click(function() {
-                $('form').children().filter('div').css('opacity', '0.7');
+            var selector = 'div:not(:last-child)';
+            $('form').children().filter(selector).click(function() {
+                $('form').children().filter(selector).css('opacity', '0.7');
                 $(this).children().filter('input').prop('checked', true);
                 $(this).css('opacity', '1');
             });
